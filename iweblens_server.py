e12c813b3514113dd5454ea5ffe4e405076036be
Chris Onsiong <christopher@Chriss-MacBook-Pro.local>
2021-04-06 10:49:53 +1000

    [add flask webserver script]

---
+++ iweblens_server.py
@@ -0,0 +1,58 @@
+import base64
+from io import BytesIO
+from PIL import Image
+import json
+import object_detection as od
+import numpy as np
+import cv2
+import sys
+from flask import Flask,jsonify, request, Response
+
+app = Flask(__name__)
+
+@app.route('/api/object_detection', methods=['POST'])
+def image_validation():
+	error = None
+	if request.method == "POST":
+		data = json.loads(request.json)
+		id = data['id']
+		resp_arr = {}
+		image_data = data['image']
+		processim = np.fromstring(base64.b64decode(image_data), dtype=np.uint8)
+		img = cv2.imdecode(processim, 1)
+		image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		nets = od.load_model(od.get_config("yolov3-tiny.cfg"),od.get_weights("yolov3-tiny.weights"))
+		pred = od.do_prediction(image, nets, od.get_labels("coco.names"))
+		
+		resp_arr = {
+			"id": data['id'],
+			"objects": 
+				pred
+			
+		}
+		return json.dumps(resp_arr, indent=9)
+	else:
+		error = "Invalid Request"
+		return error
+
+#def yolopathsCFG():
+#	cfgpath = "yolov3-tiny.cfg"
+#	
+#	CFG = object_detection.get_config(cfgpath)
+#	return CFG
+#
+#def yolopathsW():
+#	wpath = "yolov3-tiny.weights"
+#	Weights=object_detection.get_weights(wpath)
+#	return Weights
+#
+#def yolopathL():
+#	labelsPath = "coco.names"
+#	Labels = object_detection.get_labels(labelsPath)
+#	return Labels
+#		
+
+if __name__ == '__main__':
+	#ypaths = sys.argv[1]
+	app.debug = 1
+	app.run(host='0.0.0.0', threaded = True)
