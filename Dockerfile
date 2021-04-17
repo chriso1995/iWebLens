ce49b7e7b21f27bbb3d76564604aa1a4b1e69d2b
Christopher Onsiong <christopheronsiong@gmail.com>
2021-04-17 11:34:43 +0000

    minor changes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM ubuntu
 ENV DEBIAN_FRONTEND=noninteractive
+
 RUN apt-get update && \
     apt-get upgrade && \
     apt-get -y install python3 python3-pip python3-opencv libgtk2.0-dev
 
-COPY *.py /src/
-WORKDIR /src
+COPY *.py /source/
+WORKDIR /source
 
-RUN pip3 install flask numpy opencv-python opencv-contrib-python requests
+RUN pip3 install flask numpy opencv-contrib-python requests
+ADD https://pjreddie.com/media/files/yolov3-tiny.weights yolo_tiny_configs/yolov3-tiny.weights
 ADD https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names yolo_tiny_configs/coco.names
 ADD https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.cfg yolo_tiny_configs/yolov3-tiny.cfg
-ADD https://pjreddie.com/media/files/yolov3-tiny.weights yolo_tiny_configs/yolov3-tiny.weights
+
 
 EXPOSE 8080
 CMD ["python3", "iweblens_server.py"]
--- iweblens_server.py
+++ iweblens_server.py
@@ -9,21 +9,21 @@ app = Flask(__name__)
 
 @app.route('/api/object_detection', methods=['POST'])
 def image_validation():
-	data = json.loads(request.json)
+	data = json.loads(request.json) # assigning the request data into data variable
 	id = data['id']
 	resp_arr = {}
-	image_data = data['image']
-	processim = np.fromstring(base64.b64decode(image_data), dtype=np.uint8)
-	img = cv2.imdecode(processim, 1)
-	image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	image_data = data['image'] # assigning the encoded image information to variable image_data
+	processim = np.fromstring(base64.b64decode(image_data), dtype=np.uint8) # performing the decoding of the image using the base64 package
+	img = cv2.imdecode(processim, 1) # using cv2 to decode the image data and decodes it to an image format
+	image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # performing colour conversino from BGR to RGB
 	nets = od.load_model(od.get_config("yolov3-tiny.cfg"),od.get_weights("yolov3-tiny.weights"))
-	pred = od.do_prediction(image, nets, od.get_labels("coco.names"))
+	pred = od.do_prediction(image, nets, od.get_labels("coco.names")) # using the object detection script to perform the load_model and do prediction methods
 	resp_arr = {
 		"id": data['id'],
 		"objects": 
 			pred
 	}
-	return json.dumps(resp_arr, indent=9)
+	return json.dumps(resp_arr, indent=9) # returning the pred variable which is then  converting python object into json string with proper indentation
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080, threaded = True)
+	app.run(host='0.0.0.0', port=8080, threaded = True) # Threaded should be set to true by default with flask versions greater than 1.0: added for visibility
